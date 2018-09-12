c2cddacd37d5092d7678c7cf20b3be6c26a667ed
Ismail Adiputra <adiputra.ismail@gmail.com>
2018-09-12 10:16:35 -0500

    Initial commit.

---
+++ vox-headlines.py
@@ -0,0 +1,8 @@
+"""
+Author: Ismail Adiputra
+Date  : Sep. 12, 2018
+
+This is a script to collect top stories headlines from the vox website, which then will be
+tweeted through a tweet bot.
+"""
+
