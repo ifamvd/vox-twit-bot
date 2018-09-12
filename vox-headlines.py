5657650af176c737cc8f01867646edd56a692b73
Ismail Adiputra <adiputra.ismail@gmail.com>
2018-09-12 11:52:51 -0500

    Started working.

--- vox-headlines.py
+++ vox-headlines.py
@@ -6,3 +6,15 @@ This is a script to collect top stories headlines from the vox website, which th
 tweeted through a tweet bot.
 """
 
+import requests
+import os
+from bs4 import BeautifulSoup as bs
+
+# vox homepage url
+url = 'https://www.vox.com'
+# download the vox homepage
+page = requests.get(url)
+soup = bs(page.text, 'html.parser')
+# find the top stories headlines
+stories = soup.find("div", class_="c-newspaper__main")
+print(stories)
